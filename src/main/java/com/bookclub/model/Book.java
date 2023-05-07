50780f70d553f03d17d9918daaa8d9fd97edbf5c
sabinashrst <sabshrestha1@my365.bellevue.edu>
2023-05-07 07:44:54 -0500

    Week 8 Assignment

--- src/main/java/com/bookclub/model/Book.java
+++ src/main/java/com/bookclub/model/Book.java
@@ -1,72 +1,73 @@
 package com.bookclub.model;
 
-import java.util.List;
-
 public class Book {
 
 	private String isbn;
 	private String title;
 	private String description;
+	private String infoUrl;
 	private int numOfPages;
-	private List<String> authors;
 
 	public Book() {
-
 	}
 
-	public Book(String isbn, String title, String description, int numOfPages, List<String> authors) {
-		super();
+	public Book(String isbn, String title, String description, String infoUrl, int numOfPages) {
 		this.isbn = isbn;
 		this.title = title;
 		this.description = description;
+		this.infoUrl = infoUrl;
 		this.numOfPages = numOfPages;
-		this.authors = authors;
 	}
 
-	public String getIsbn() {
-		return isbn;
+	public Book(String isbn, String title, String infoUrl) {
+		this.isbn = isbn;
+		this.title = title;
+		this.infoUrl = infoUrl;
 	}
 
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
 
-	public String getTitle() {
-		return title;
+	public String getIsbn() {
+		return isbn;
 	}
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-	public String getDescription() {
-		return description;
+	public String getTitle() {
+		return title;
 	}
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-	public int getNumOfPages() {
-		return numOfPages;
+	public String getDescription() {
+		return description;
 	}
 
 	public void setNumOfPages(int numOfPages) {
 		this.numOfPages = numOfPages;
 	}
 
-	public List<String> getAuthors() {
-		return authors;
+	public int getNumOfPages() {
+		return numOfPages;
 	}
 
-	public void setAuthors(List<String> authors) {
-		this.authors = authors;
+	public void setInfoUrl(String infoUrl) {
+		this.infoUrl = infoUrl;
+	}
+
+	public String getInfoUrl() {
+		return infoUrl;
 	}
 
 	@Override
 	public String toString() {
-		return "Book [isbn=" + isbn + ", title=" + title + ", description=" + description + ", numOfPages=" + numOfPages
-				+ ", authors=" + authors + "]";
+		return String.format("Book{isbn=%s, title=%s, description=%s, infoUrl=%s, numOfPages=%s}", isbn, title,
+				description, infoUrl, numOfPages);
 	}
-
 }
