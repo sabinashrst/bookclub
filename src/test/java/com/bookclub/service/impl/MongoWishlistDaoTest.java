e8c15923a06b62d125d66382305a42b8640a75d8
sabshres <neba_sabishres22s@yahoo.com>
2023-06-01 11:47:05 -0500

    Fixed unit test

--- src/test/java/com/bookclub/service/impl/MongoWishlistDaoTest.java
+++ src/test/java/com/bookclub/service/impl/MongoWishlistDaoTest.java
@@ -1,7 +1,8 @@
 package com.bookclub.service.impl;
 
-import static org.junit.Assert.assertTrue;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
